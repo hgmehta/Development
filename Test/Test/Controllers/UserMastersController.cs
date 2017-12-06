a1d0cb853c25b0bd964b8efc483a9655694ab82f
Harsh Mehta <harsh_m@hotmail.com>
2017-12-06 20:20:32 +0530

    Learning Entity Framework

---
+++ Test/Test/Controllers/UserMastersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class UserMastersController : Controller
+    {
+        private specIndiaDBContext db = new specIndiaDBContext();
+
+        // GET: UserMasters
+        public ActionResult Index()
+        {
+            return View(db.UserMasters.ToList());
+        }
+
+        // GET: UserMasters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserMaster userMaster = db.UserMasters.Find(id);
+            if (userMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userMaster);
+        }
+
+        // GET: UserMasters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserMasters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Firstname,MiddleName,Lastname,Gender,Birthdate,Address,City,EmailId,Password,ConfirmPassword")] UserMaster userMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserMasters.Add(userMaster);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userMaster);
+        }
+
+        // GET: UserMasters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserMaster userMaster = db.UserMasters.Find(id);
+            if (userMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userMaster);
+        }
+
+        // POST: UserMasters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Firstname,MiddleName,Lastname,Gender,Birthdate,Address,City,EmailId,Password")] UserMaster userMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userMaster).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userMaster);
+        }
+
+        // GET: UserMasters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserMaster userMaster = db.UserMasters.Find(id);
+            if (userMaster == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userMaster);
+        }
+
+        // POST: UserMasters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserMaster userMaster = db.UserMasters.Find(id);
+            db.UserMasters.Remove(userMaster);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
